5b2d88bbe9e4344e410b643ab37d61a54cff9ad5
+ <+>
2023-10-22 02:40:31 +0300

    models and model relationship

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -6,5 +6,27 @@ class Hero(db.Model):
     __tablename__ = 'hero'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    super_name = db.Column(db.String(255), nullable=False)
 
-# add any models you may need. 
+    powers = db.relationship("Power", secondary="hero_powers", back_populates="heroes")
+
+class Power(db.Model):
+    __tablename__ = "powers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+
+    heroes = db.relationship("Hero", secondary="hero_powers", back_populates="powers")
+
+class HeroPower(db.Model):
+    __tablename__ = "hero_powers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String(20), nullable=False)
+    
+    hero_id = db.Column(db.Integer, db.ForeignKey("heroes.id"), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey("powers.id"), nullable=False)
+    hero = db.relationship("Hero", backref=backref("hero_powers", cascade="all, delete-orphan"))
+    power = db.relationship("Power", backref=backref("hero_powers", cascade="all, delete-orphan"))
